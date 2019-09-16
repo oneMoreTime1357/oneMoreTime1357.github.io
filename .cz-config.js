b15cc0fec83e87076d3fea543422cc3069f15bc9
yangdan <1345335516@qq.com>
2019-09-16 21:31:12 +0800

    refactor: modify setting
    modify github commit

---
+++ .cz-config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+
+  types: [
+    {
+      value: 'article',
+      name : '🦉  article:      Add new article'
+    },
+    {
+      value: 'update',
+      name : '🐳  update:      Update article'
+    },
+    {
+      value: 'fix',
+      name : '🐞  fix:      A bug fix'
+    },
+    {
+      value: 'refactor',
+      name : '🛠  refactor: A code change that neither fixes a bug nor adds a feature'
+    },
+    {
+      value: 'docs',
+      name : '📚  docs:     Documentation only changes'
+    },
+    {
+      value: 'style',
+      name : '🌈  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+    },
+    {
+      value: 'revert',
+      name : '⏪  revert:   Revert to a commit'
+    }
+  ],
+
+  scopes: [],
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix"]
+};
